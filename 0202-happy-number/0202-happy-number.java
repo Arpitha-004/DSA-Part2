788e28ad4be47e2701aab90290c59f12ffcc44f1
Arpitha S <145808037+Arpitha-004@users.noreply.github.com>
2025-08-30 01:14:04 +0530

    Time: 1 ms (64.5%), Space: 40.7 MB (76%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,16 +1,18 @@
 class Solution {
     public boolean isHappy(int n) {
+        int slow=n,fast=n;
+        do{
+            slow = sqdig(slow);
+            fast = sqdig(sqdig(fast));
+        }while(slow!=fast);
+        return slow==1;
+    }
+    public int sqdig(int n){
         int sum=0;
-        Set<Integer> set = new HashSet<>();
-        while(sum!=1 && !set.contains(sum)){
-            set.add(sum);
-            sum=0;
             while(n!=0){
                 sum+=Math.pow(n%10,2);
                 n=n/10;
             }
-            n=sum;
-        }
-        return sum==1;
+        return sum;
     }
 }
